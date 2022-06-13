28bb2efe9c6b1171c30a540f2f5eb6b6a579261e
Seimouun <simon.reitmann>
2022-06-13 15:53:16 +0200

    stinky

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+
+mysql.createConnection({
+    user: ''
+})
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    res.send('Hello World' + req.params.)
 })
 
 app.listen(3000)
