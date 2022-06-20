a1d93b5dfd46774fa7db17cf23176855bf1c82a0
Seimouun <simon.reitmann>
2022-06-20 16:28:07 +0200

    master of disaster

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql')
 
-mysql.createConnection({
-    user: ''
+let connection = mysql.createConnection({
+    host: '10.10.0.9',
+    port: 1521,
+    user: 'C##SREITMANN18',
+    password: ''
 })
 
+connection.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+    console.log('Connected to the MySQL server.');
+});
+
 app.get('/', function (req, res) {
-    res.send('Hello World' + req.params.)
+    res.send('Hello World' + req.params)
 })
 
 app.listen(3000)
